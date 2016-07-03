930106a22a57bce6018160ffa09d0392448389dc
varen1994 <varendersingh130@gmail.com>
2016-07-03 12:15:34 +0530

    Is Element Present Method added

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -16,6 +16,7 @@ class BinaryTree  {
   void insert_node(int data); 
   struct node *method_to_find_parent(struct node *root,int data);
   void traverseTree(struct node *root);
+  bool is_Element_Present(struct node *root,int data);
 
 };
  
@@ -68,8 +69,22 @@ class BinaryTree  {
            traverseTree(root->left);
            cout<<root->data<<" ";
            traverseTree(root->right); 
-         }    
+         }   
+       return ; 
     }
+
+    bool BinaryTree::is_Element_Present(struct node *root,int data)  { 
+       static bool status_is_found = false; 
+       if(root!=NULL)  {
+           if(root->data==data)  {
+              status_is_found = true;
+           }
+           is_Element_Present(root->left,data); 
+           is_Element_Present(root->right,data); 
+        }
+      return status_is_found; 
+    }
+
 
 int main()  {
   BinaryTree Tree;
@@ -81,5 +96,8 @@ int main()  {
   Tree.insert_node(120);
   Tree.insert_node(101);
   Tree.traverseTree(Tree.root);
+
+  cout<<"status is \n"<<Tree.is_Element_Present(Tree.root,1111)<<"\n";
+
   return 0;
 } 
