fcfc08ba1f299d6f6694f1f709cd1f34fceb9210
varen1994 <varendersingh130@gmail.com>
2016-07-08 21:12:47 +0530

    Map.cpp got modified

--- Map.cpp
+++ Map.cpp
@@ -8,14 +8,29 @@ class Dictionary  {
   map<string,long int>List;
   void addValueToMap()  {
      List.insert(std::make_pair("varender",128093));
+     List.insert(std::make_pair("Rohan",128067));
+     List.insert(std::make_pair("Sudhanshu",128083));
      List["varender"] = List["varender"]+12; 
      cout<<List["varender"]; 
      return ; 
    } 
+
+   void traverseMap()  {
+     cout<<"\n"; 
+     map<string,long int>::iterator i;
+     for(i=List.begin();i!=List.end();++i)  {
+         cout<<"{"<<(*i).first<<":";
+         cout<<(*i).second<<"}";
+         cout<<"\n"; 
+      }
+     
+       return ;
+   }
 };
 
 int main()  {
   Dictionary dict;
   dict.addValueToMap();
+  dict.traverseMap(); 
   return 0;
 }
